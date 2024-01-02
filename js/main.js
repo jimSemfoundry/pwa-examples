832e3e1a503f6fada018fd61509fa5164766fe8d
lijianjun <574891945@qq.com>
2024-01-02 14:35:01 +0800

    推送

--- js/main.js
+++ js/main.js
@@ -1,8 +1,139 @@
 window.onload = () => {
-  'use strict';
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js",{
+        scope: ".", // <--- THIS BIT IS REQUIRED
+    })
+    .then(async function (registration) {
+        // console.log("Service Worker registered successfully:", registration);
+        // subscribeButton.addEventListener("click", function () {
+            subscribeToPushNotifications(registration);
+        // });
+  
+        // unsubscribeButton.addEventListener("click", function () {
+        //     unsubscribeFromPushNotifications(registration);
+        // });
+        // registration.pushManager.getSubscription().then(function (subscription) {
+        //     isSubscribed = !(subscription === null);
+        //     if (isSubscribed) {
+        //         console.log('User IS subscribed.');
+        //     } else {
+        //         console.log('User is NOT subscribed.');
+        //         subscribeToPushNotifications(registration)
+        //     }
+        // });
+  
+    })
+    .catch(function (error) {
+        console.log("Service Worker registration failed:", error);
+    });
+  }
+  
+  async function subscribeToPushNotifications(registration) {
+    // const SERVER_URL = "https://jim-api.123998.me/jimapi/getPublicKey";
+    const SERVER_URL = "https://lark.semfoundry.com/api/LarkInform/get_public_key";
+    fetch(SERVER_URL, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        })
+        .then(response => {
+            response.json().then((data) => {
+                registration.pushManager
+                    .subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: urlBase64ToUint8Array(data.publicKey),
+                    })
+                    .then(function (subscription) {
+                        // console.log("Subscribed to push notifications:", subscription);
+                        // updateSubscriptionOnServer(subscription);
+                        updateSubscriptionOnServerToMike(subscription)
+                        // subscribeButton.disabled = true;
+                        // unsubscribeButton.disabled = false;
+                    })
+                    .catch(function (error) {
+                        console.log("Failed to subscribe to push notifications:", error);
+                        // subscribeButton.disabled = false;
+                        // unsubscribeButton.disabled = true;
+                    });
+            }).catch((err) => {
+                console.error('get publicKey Failed')
+            })
+        })
+    
+  }
+  
+  function unsubscribeFromPushNotifications(registration) {
+    registration.pushManager.getSubscription().then(function (subscription) {
+        if (subscription) {
+            subscription
+                .unsubscribe()
+                .then(function () {
+                    console.log("Unsubscribed from push notifications:", subscription);
+                    updateSubscriptionOnServer(null);
+                    updateSubscriptionOnServerToMike(null)
+                    // subscribeButton.disabled = false;
+                    // unsubscribeButton.disabled = true;
+                })
+                .catch(function (error) {
+                    console.log("Failed to unsubscribe from push notifications:", error);
+                    // subscribeButton.disabled = true;
+                    // unsubscribeButton.disabled = false;
+                });
+        }
+    });
+  }
+  
+  function getBrowserLang() {
+    let browserLang = navigator.language ? navigator.language : navigator.browserLanguage;
+    let defaultBrowserLang = ''
+    if (browserLang.indexOf('es') > -1){
+        defaultBrowserLang = 'es'
+    } else if (browserLang.indexOf('pt') > -1){
+        defaultBrowserLang = 'pt'
+    } else {
+        defaultBrowserLang = 'en'
+    }
+    return defaultBrowserLang
+  }
+  
+  async function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to server for storage and use
+    // console.log(subscription)
+    const SERVER_URL = "https://jim-api.123998.me/jimapi/save-subscription";
+    const response = await fetch(SERVER_URL, {
+        method: "post",
+        headers: {
+            "Content-Type":"application/x-www-form-urlencoded",
+        },
+        body: 'id=' + JSON.stringify(subscription)
+    });
+    return response.json();
+  }
+  
+  
+  async function updateSubscriptionOnServerToMike(subscription) {
+    // TODO: Send subscription to server for storage and use
+  
+    const SERVER_URL = "https://lark.semfoundry.com/api/LarkInform/save_subscription";
+    const response = await fetch(SERVER_URL, {
+        method: "post",
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body:'id=' + JSON.stringify(subscription)+'&language=' + getBrowserLang()
+    });
+    return response.json();
+  }
+  
+  function urlBase64ToUint8Array(base64String) {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
   }
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'betfrom';
 var filesToCache = [
   '/',
   '/index.html',
@@ -24,4 +24,52 @@ self.addEventListener('fetch', function(e) {
       return response || fetch(e.request);
     })
   );
+});
+
+let json = {
+  'msgUrl':''
+}
+
+self.addEventListener("push",  (event) => {
+  // console.log("Received a push message", event);
+  // console.log(new Date().getTime())
+  // console.log(event.data.json())
+  // console.log(event.data.text())
+
+  const data = event.data.json()
+  json = event.data.json()
+
+  const title = data.title;
+
+  const options = {
+    body: data.body,
+    icon: data.icon,
+    image: data.image,
+    badge: data.icon,
+    actions: data.actions
+  };
+  // console.log(options)
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+self.addEventListener("notificationclick",  (event) => {
+  // console.log("Notification clicked", event);
+  // console.log(event)
+  // console.log(json)
+  // event.notification.close();
+  // event.waitUntil(clients.openWindow(json["msg-url"]));
+
+  event.notification.close();
+    if (event.action === '' ){
+      event.waitUntil(clients.openWindow(json.msgUrl));
+    }else{
+      if ('actions' in json){
+        json.actions.filter(item => {
+          if (event.action === item.action) {
+            event.waitUntil(clients.openWindow(item.msgUrl));
+          }
+        })
+      }
+    }
 });
